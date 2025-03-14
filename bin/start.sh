4d331b31e9c2f0768ea7951facea5ae2f95e7f25
Adam Martula <martulaadam@gmail.com>
2025-03-14 10:21:40 +0100

    Improve the start script
    The start script has been improved to include auto-completion for the path input for better comfort.

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+# Take the script location and generate an absolute path out of it
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-echo Please specify the PID file path:
-read serverstatuscheckerpathvar
-echo The specified path was: $serverstatuscheckerpathvar
-java -jar "$SCRIPT_DIR/serverstatuschecker.jar" $serverstatuscheckerpathvar --spring.config.location="$SCRIPT_DIR/application.properties"
+# Ask the user for the path to the file with the PIDs
+echo You are at: $(pwd)
+# -e enables autocompletion for more comfort
+read -e -p "Please specify the PID file path: " PIDPATHVAR
+# Echo the user his input
+echo The specified path was: $PIDPATHVAR
+# Run the program
+java -jar "$SCRIPT_DIR/serverstatuschecker.jar" $PIDPATHVAR --spring.config.location="$SCRIPT_DIR/application.properties"
