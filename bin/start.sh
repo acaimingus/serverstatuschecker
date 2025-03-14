1f4248760a7469db5241e68353075707f27b2220
Adam Martula <martulaadam@gmail.com>
2025-03-14 10:03:42 +0100

    Fixed start script

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 echo Please specify the PID file path:
 read serverstatuscheckerpathvar
 echo The specified path was: $serverstatuscheckerpathvar
-java -jar ./serverstatuschecker.jar $serverstatuscheckerpathvar --spring.config.location=./application.properties
+java -jar "$SCRIPT_DIR/serverstatuschecker.jar" $serverstatuscheckerpathvar --spring.config.location="$SCRIPT_DIR/application.properties"
